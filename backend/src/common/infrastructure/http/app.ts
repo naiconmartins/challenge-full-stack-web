f4aa92113e8ebaf43396b480e86c460bcad13033
user.name like: "Naicon Martins <user.email like: "naiconmartins@hotmail.com>
2026-03-20 19:33:48 -0400

    feat: add domain error classes, global error handler middleware, and multer dependency

--- backend/src/common/infrastructure/http/app.ts
+++ backend/src/common/infrastructure/http/app.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
+import { errorHandler } from "./middleware/errorHandlers";
 import { router } from "./route";
 
 export const app: Express = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
