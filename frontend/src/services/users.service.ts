5383d2cd3a78190825ad7228a2dd14d965d8db10
user.name like: "Naicon Martins <user.email like: "naiconmartins@hotmail.com>
2026-03-23 10:37:14 -0400

    refactor(legacy): remove obsolete student helper files and unused service stubs

--- frontend/src/services/users.service.ts
+++
@@ -1,9 +0,0 @@
-import { httpClient } from './http'
-import type { CreateUserDto, User } from '@/types/user'
-
-export const usersService = {
-  async create(data: CreateUserDto): Promise<User> {
-    const response = await httpClient.post<User>('/users', data)
-    return response.data
-  },
-}
