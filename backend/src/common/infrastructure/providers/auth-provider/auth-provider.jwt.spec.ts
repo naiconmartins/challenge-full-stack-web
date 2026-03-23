4349e515a1e67c96e88a3d1399a260661cc85e73
user.name like: "Naicon Martins <user.email like: "naiconmartins@hotmail.com>
2026-03-23 11:26:32 -0400

    test(auth): add JwtAuthProvider unit tests

---
+++ backend/src/common/infrastructure/providers/auth-provider/auth-provider.jwt.spec.ts
@@ -0,0 +1,34 @@
+import { UnauthorizedError } from "@/common/domain/errors/unauthorized-error";
+import { UsersDataBuilder } from "@/modules/users/testing/helpers/users-data-builder";
+import "reflect-metadata";
+import { JwtAuthProvider } from "./auth-provider.jwt";
+
+describe("JwtAuthProvider", () => {
+  let sut: JwtAuthProvider;
+
+  beforeEach(() => {
+    sut = new JwtAuthProvider();
+  });
+
+  it("should generate a token containing user id and role", () => {
+    const user = UsersDataBuilder({ role: "ATTENDANT" });
+    const { access_token } = sut.generateAuthKey({
+      user_id: user.id,
+      role: user.role,
+    });
+
+    const result = sut.verifiyAuthKey(access_token);
+
+    expect(access_token).toBeDefined();
+    expect(result).toStrictEqual({
+      user_id: user.id,
+      role: user.role,
+    });
+  });
+
+  it("should throw UnauthorizedError when token is invalid", () => {
+    expect(() => sut.verifiyAuthKey("invalid.token")).toThrow(
+      UnauthorizedError,
+    );
+  });
+});
